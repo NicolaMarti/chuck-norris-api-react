2f36edd30c471494064c5bf302ddd66c9b660d83
Vito <desandi.vito@panettipitagora.edu.it>
2023-03-23 09:41:44 +0100

    Conclusione Progetto
    Aggiustati file CSS , Aggiunta dei file Dropdown.jsx e JokeText.jsx con apposite modifiche e CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,60 @@
 import { useState } from 'react'
 import './styles/App.css'
-import Title from './components/Title'
-import Button from './components/Button'
+import Title from './components/Title.jsx'
+import Button from './components/Button.jsx'
+import Dropdown from './components/Dropdown.jsx'
+import JokeText from './components/JokeText.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [joke, setJoke] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [clicked, setClicked] = useState(false);
+  let url = "https://api.chucknorris.io/jokes/categories";
+  fetch(url).then((resp)=>{
+    return resp.json();
+  }).then(data=>{
+    data.unshift("random");
+    setCategories(data);
+  }).catch((e)=>{
+    console.log(e)
+  })
 
-  const [joke , setJoke ]= useState(null)
+  function generateJoke(){
+    let selectedCategory = document.getElementById("dropdown").value
+    let url = "https://api.chucknorris.io/jokes/" + (selectedCategory==="random"?"random":("random?category=") + selectedCategory);
+    fetch(url).then((resp)=>{
+      return resp.json();
+    }).then(data=>{
+      setJoke(data.value);
+      setClicked(true);
+    }).catch((e)=>{
+      console.log(e)
+    })
+  }
+
+  function copyJoke(){
+    let jokeText = document.getElementById("JokeText");
+    navigator.clipboard.writeText(jokeText.innerText);
+  }
 
   return (
     <div className="App">
-     <title>CIAOO</title>
-     <h1>Benvenuti nel sito di Chuck Norris</h1>
-     <p></p>
-     <div className="card">
-      <button onClick={( )=> setCount((count)=>count+1)}>
-        count is {count}
-      </button>
-      <Button text="Carica il Joke"></Button>
-      <Button text="Copia"></Button>
-     </div>
-
-
+      <Title>Chuck Norris Joke Generator</Title>
+      <table>
+        <tr>
+          <td>
+            <Button id='generate' callback={generateJoke}>Generate Joke</Button>
+            <Dropdown id='dropdown' values={categories}></Dropdown>
+          </td>
+          <td>
+            <JokeText id='JokeText'>{joke}</JokeText>
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><Button id='clipboard' callback={copyJoke} variant={clicked === true ? undefined : "disabled"}>Copy to clipboard</Button></td>
+        </tr>
+      </table>
     </div>
   )
 }
---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,16 @@
+
+import { useState } from 'react'
+import '../styles/Dropdown.css'
+
+function Dropdown(props) {
+
+  return (
+    <div className="Dropdown">
+        <select id={props.id}>
+        {props.values.map((el,index)=> {return <option value={el} key={index}>{el}</option>})}
+        </select>
+    </div>
+  )
+}
+
+export default Dropdown
